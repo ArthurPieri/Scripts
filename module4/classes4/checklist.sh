1ee02dc21bead52f2f329952eb3449f22e82db9a
Arthur Pieri <arthurpieri@gmail.com>
2018-01-01 17:15:45 -0200

    Creating folders for module 4 and Checklist script

---
+++ module4/classes4/checklist.sh
@@ -0,0 +1,19 @@
+#! /bin/bash
+#Shebang Ĩ
+echo "First you have to put the Shebang wich in this case is /bin/bash"
+echo "------------------"
+#Comments / File header
+echo "Then you may create a File Header with your name an some explanaition"
+echo "------------------"
+#Global variables
+echo "Third you declare all your global variables"
+echo "------------------"
+#Functions
+echo "Fourth declare your functions"
+echo "------------------"
+#Main script contents
+echo "Fithy you do the script itself"
+echo "------------------"
+#Exit with an exit status
+echo "And last you exit with an exit status so you know it ended correctly"
+echo "------------------"
