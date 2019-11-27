3bca7acc71fbdadfea42b6f5bdc56be14a4e2b1b
Arthur Pieri <19278543+ArthurPieri@users.noreply.github.com>
2019-11-27 14:58:33 -0300

    Create gitpush.sh

---
+++ Git/gitpush.sh
@@ -0,0 +1,75 @@
+#! /bin/bash
+#
+# /HEADER/
+# This script is used to automaticly pull all changes from the git repository
+# This script is currently on v0.1
+# Author: Arthur Pieri
+#
+#-------------------------------------------------------------------------------
+# Entering the coding folder to start pulling
+cd ~/code
+# ------------------------------------------------------------------------------
+cd conceitos_Programacao/
+echo Conceitos programacao
+git add .
+git commit -m "Automatic Push from script"
+git push
+echo ----------------------
+
+cd ..
+cd UnB/
+echo UnB
+git add .
+git commit -m "Automatic Push from script"
+git push
+#npm i
+echo ----------------------
+
+cd ..
+cd Gestao/
+echo Gestao
+git add .
+git commit -m "Automatic Push from script"
+git push
+#npm i
+echo ----------------------
+
+cd ..
+cd ChatApp-udemy/
+echo Chat Uol
+git add .
+git commit -m "Automatic Push from script"
+git push
+echo ----------------------
+
+cd ..
+cd  chatbot/
+echo Chatbot
+git add .
+git commit -m "Automatic Push from script"
+git push
+echo ---------------------
+
+cd ..
+cd siteArthur/
+echo Site Arthur
+git add .
+git commit -m "Automatic Push from script"
+git push
+echo ---------------------
+
+cd ..
+cd Scripts/
+echo Usefull Scripts
+git add .
+git commit -m "Automatic Push from script"
+git push
+echo --------------------
+
+cd ..
+cd atlasbot
+echo Atlas Bot
+git add .
+git commit -m "Automatic Push from script"
+git push
+echo --------------------
