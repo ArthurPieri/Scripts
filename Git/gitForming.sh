1ad7cddac4dbe18e5a2214c965985c23ba52cf22
Arthur Pieri <git@arthurpieri.com>
2020-02-14 15:25:38 -0300

    Removing OS check and Debug mode for GIT script

--- Git/gitForming.sh
+++ Git/gitForming.sh
@@ -6,39 +6,6 @@
 # Author: Arthur Pieri
 #
 #-------------------------------------------------------------------------------
-# OS check so it only runs on GNU/Linux
-#-------------------------------------------------------------------------------
-OS_CHECK=$(uname -o)
-if [ "${OS_CHECK}" != "GNU/Linux" ]
-then
-    echo "This script was made for the GNU/Linux system you are using: ${OS_CHECK}"
-    exit 1
-fi
-#-------------------------------------------------------------------------------
-# Checking with the CPU architecture is correct
-#-------------------------------------------------------------------------------
-CPU_ARC_CHECK=$(uname -m)
-if [ "${CPU_ARC_CHECK}" != "x86_64" ]
-then
-    echo "This script was made for x86 system, it seems you're using: ${CPU_ARC_CHECK} system"
-    exit 2
-fi
-#-------------------------------------------------------------------------------
-# Verifying if debug mode on
-#-------------------------------------------------------------------------------
-if [ "${1}" = "debug" ]
-then
-    echo "Debug mode on"
-    sleep 5
-# When using -x the PS4 is the variable printed to the screen wich by default is: +
-# You can change the value of PS4
-    PS4='+ ${BASH_SOURCE} - line:${LINENO} : '
-    echo "Test"
-# Set -x start the debug mode
-    set -x
-fi
-
-#-------------------------------------------------------------------------------
 # Verifying if git exists on the computer
 #-------------------------------------------------------------------------------
 git --version
