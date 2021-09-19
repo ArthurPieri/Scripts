67e1e667c43ee271cff23ca630f00ce89e759406
Arthur Pieri <git@arthurpieri.com>
2021-09-19 03:02:35 +0000

    Just some aesthetic changes

--- VPS/ssh-add.sh
+++ VPS/ssh-add.sh
@@ -5,8 +5,13 @@
 # This script is currently on v1
 # Author: Arthur Pieri
 
+echo --------------------
 echo "Starting ssh-agent"
+echo --------------------
 eval `ssh-agent -s`
 
+echo --------------------
 echo "Adding private key to ssh-agent"
+echo --------------------
 ssh-add ~/.ssh/id_ed25519_ssh
+echo --------------------
