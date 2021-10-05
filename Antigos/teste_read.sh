178e86db449425641696e1251501220c87842a8c
Arthur Pieri <git@arthurpieri.com>
2021-10-04 22:29:29 -0300

    Setting nginx server blocks and certbot

--- Antigos/teste_read.sh
+++ Antigos/teste_read.sh
@@ -1,11 +1,61 @@
 #! /bin/bash
 
-echo Did you run createuser.sh script first?
-read -p "Are you sure? (y or n): " -n 1 -r
-echo    # (optional) move to a new line
-if [[ ! $REPLY =~ ^[Yy]$ ]]
-then
-    exit 1
-fi
+#echo Did you run createuser.sh script first?
+#read -p "Are you sure? (y or n): " -n 1 -r
+#echo    # (optional) move to a new line
+#if [[ ! $REPLY =~ ^[Yy]$ ]]
+#then
+#    exit 1
+#fi
 
-echo YEXX
+#echo YEXX
+
+#echo digite o dominio que deseja criar o Server Block
+#read var
+#echo $var
+
+#mkdir ~/code/$var
+
+#touch ~/code/$var/teste.html
+
+#echo "<html>
+#    <head>
+#        <title>Welcome to $domain_name!</title>
+#    </head>
+#    <body>
+#        <h1>Success!  The $domain_name server block is working!</h1>
+#    </body>
+#    </html>" >> ~/code/$var/teste.html
+
+#var='n'
+#echo $var
+
+#answer='n'
+#    while [[ ! $answer =~ ^[Yy]$ ]]
+#    do 
+#        echo -----------------------------------
+#        echo Please insert your domain name here
+#        echo -----------------------------------
+#        read domain_name
+#        echo "is $domain_name correct (y or n)"?
+#        read answer
+#        echo 
+#    done
+
+#touch $domain_name.txt
+
+#nano $domain_name.txt
+
+#echo lalalala
+
+#echo "teste
+#arthur
+#jose
+##mano
+#lalala
+#" >> teste.txt
+
+#filename="teste.txt"
+#search="#mano"
+#replace="Aragorn"
+#sed -i "s/$search/$replace/" $filename
