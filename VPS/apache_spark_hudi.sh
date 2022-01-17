1dd53a00d53d9d652c7b708cd3d84a9cdfc561a9
Arthur Pieri <git@arthurpieri.com>
2022-01-17 17:06:48 -0300

    Automatic Push from script

--- VPS/apache_spark_hudi.sh
+++ VPS/apache_spark_hudi.sh
@@ -23,5 +23,7 @@ echo "Setting up the variables"
     echo "export SPARK_HOME=/opt/spark" >> ~/.bashrc
     echo "export PATH=$PATH:$SPARK_HOME/bin:$SPARK_HOME/sbin" >> ~/.bashrc
     echo "export PYSPARK_PYTHON=/usr/bin/python3" >> ~/.bashrc
+echo "Installing Hudi packages"
+    spark-shell --packages org.apache.hudi:hudi-spark-bundle_2.12:0.10.0,org.apache.spark:spark-avro_2.12:2.4.4 --conf 'spark.serializer=org.apache.spark.serializer.KryoSerializer'
 
-
+    # spark-shell --packages org.apache.hudi:hudi-spark3-bundle_2.12:0.10.0,org.apache.spark:spark-avro_2.12:3.1.2 --conf 'spark.serializer=org.apache.spark.serializer.KryoSerializer'
